5885af317b12f11f3a11d840d54866a849d9a281
Vladimir Krygin <nif315@gmail.com>
2019-12-02 22:49:38 +0300

    Create Phone Number kata uploaded

---
+++ Create Phone Number.dart
@@ -0,0 +1,20 @@
+String createPhoneNumber(List numbers) {
+  String areaCode = "";
+  String phoneNumber = " ";
+
+  for (int i = 0; i < 3; i++) {
+    areaCode += numbers[i].toString();
+  }
+
+  for (int i = 3; i < 10; i++) {
+    phoneNumber += numbers[i].toString();
+
+    if (i == 5) phoneNumber += "-" + numbers[i].toString();
+  }
+  return "(" + areaCode + ")" + phoneNumber;
+}
+
+void main() {
+  List numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  print(createPhoneNumber(numbers));
+}
